e30fcbb760ab8df3d9cfffa431ad227fef3d3ecd
BrunoDeNadaiSarnaglia <brunodenadaisarnaglia@gmail.com>
2016-10-24 01:50:24 -0200

    Area Polygon

---
+++ src/AreaPolygon.scala
@@ -0,0 +1,23 @@
+/**
+  * Created by Bruno on 10/24/2016.
+  */
+class AreaPolygon {
+
+  def area(p1 : (Double, Double), p2: (Double, Double), p3 : (Double, Double)) : Double = {
+    Math.abs((p1._1*(p2._2 - p3._2) + p2._1*(p3._2 - p1._2) + p3._1*(p1._2 - p2._2)) / 2)
+  }
+
+  def main(args : Array[String]) ={
+    val points = (1 to readInt())
+      .map(_ => {
+        val line = readLine().split(" ")
+        (line(0).toDouble, line(1).toDouble)
+        })
+      println(points
+        .drop(1)
+        .sliding(2)
+        .map(a => area(points(0), a(0), a(1)))
+        .sum)
+  }
+
+}
